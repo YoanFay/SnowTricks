6afd590d85cf24c21d01ecf99594e392fa8e5f11
YoanFay <yoanfayolle.yf@gmail.com>
2023-09-21 14:07:54 +0200

    fixtures

--- src/DataFixtures/ImagesFixtures.php
+++ src/DataFixtures/ImagesFixtures.php
@@ -24,62 +24,74 @@ class ImagesFixtures extends Fixture implements DependentFixtureInterface
                 [
                     'tricks' => $this->getReference('mute'),
                     'name' => '01',
-                    'type' => 'jpg'
+                    'type' => 'jpg',
+                    'main' => true
                 ],
                 [
                     'tricks' => $this->getReference('mute'),
                     'name' => '02',
-                    'type' => 'jpeg'
+                    'type' => 'jpeg',
+                    'main' => false
                 ],
                 [
                     'tricks' => $this->getReference('mute'),
                     'name' => '03',
-                    'type' => 'jpg'
+                    'type' => 'jpg',
+                    'main' => false
                 ],
                 [
                     'tricks' => $this->getReference('stalefish'),
                     'name' => '01',
-                    'type' => 'jpg'
+                    'type' => 'jpg',
+                    'main' => true
                 ],
                 [
                     'tricks' => $this->getReference('japanair'),
                     'name' => '01',
-                    'type' => 'jpg'
+                    'type' => 'jpg',
+                    'main' => true
                 ],
                 [
                     'tricks' => $this->getReference('180'),
                     'name' => '01',
-                    'type' => 'jpg'
+                    'type' => 'jpg',
+                    'main' => true
                 ],
                 [
                     'tricks' => $this->getReference('bigfoot'),
                     'name' => '01',
-                    'type' => 'jpg'
+                    'type' => 'jpg',
+                    'main' => true
                 ],
                 [
                     'tricks' => $this->getReference('haakonflip'),
                     'name' => '01',
-                    'type' => 'jpg'
+                    'type' => 'jpg',
+                    'main' => true
                 ],
                 [
                     'tricks' => $this->getReference('frontflips'),
                     'name' => '01',
-                    'type' => 'jpg'
+                    'type' => 'jpg',
+                    'main' => true
                 ],
                 [
                     'tricks' => $this->getReference('backflips'),
                     'name' => '01',
-                    'type' => 'jpg'
+                    'type' => 'jpg',
+                    'main' => true
                 ],
                 [
                     'tricks' => $this->getReference('noseslide'),
                     'name' => '01',
-                    'type' => 'jpg'
+                    'type' => 'jpg',
+                    'main' => true
                 ],
                 [
                     'tricks' => $this->getReference('tailslide'),
                     'name' => '01',
-                    'type' => 'jpg'
+                    'type' => 'jpg',
+                    'main' => true
                 ],
             ];
 
@@ -88,6 +100,10 @@ class ImagesFixtures extends Fixture implements DependentFixtureInterface
             $image->setName($imageTab['name']);
             $image->setTricks($imageTab['tricks']);
             $image->setType($imageTab['type']);
+
+            if ($imageTab['main']){
+                $image->setMain(true);
+            }
 
             $manager->persist($image);
         }
